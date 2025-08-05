517b7007c7e218949c381084e242ba0c93a8f2f5
Uriartee <alvaro.uriarte10l@gmail.com>
2025-08-05 13:23:29 +0200

    Smart Summary

---
+++ src/main/java/com/uriarte/smartsummary/SmartSummaryApplication.java
@@ -0,0 +1,13 @@
+package com.uriarte.smartsummary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSummaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSummaryApplication.class, args);
+	}
+
+}
